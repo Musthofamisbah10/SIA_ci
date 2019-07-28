11ec07d5cbcf141f3104bbd530ad419201ee7770
musthofamisbah10 <musthofamisbah09@gmail.com>
2019-07-28 23:12:35 +0700

    templating ci

---
+++ application/views/templates_admin/footer.php
@@ -0,0 +1,8 @@
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?php echo base_url() ?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?php echo base_url() ?>assets/js/sb-admin-2.min.js"></script>
+  </body>
+</html>
