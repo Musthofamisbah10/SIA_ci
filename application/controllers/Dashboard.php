d7ab04cf605e0b033a4b39527f299d7ac079ee70
musthofamisbah10 <musthofamisbah09@gmail.com>
2019-07-31 17:36:05 +0700

    session dan level dashbroad

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,10 +4,18 @@
  	
  	public function index()
  	{
+ 		$data =	$this->user_model->ambil_data($this->session->userdata['username']);
+ 		
+ 		$data = array(
+ 			'username' 	=> $data->username,
+ 			'level'		=> $data->level,
+  			 );
+
  		$data['judul'] = 'Dashboard';
  		$this->load->view('templates_admin/header', $data);
  		$this->load->view('templates_admin/sidebar');
- 		$this->load->view('admin/dashboard');
+ 		$this->load->view('admin/dashboard', $data);
+ 		$this->load->view('templates_admin/footer_copyR');
  		$this->load->view('templates_admin/footer');
  	}
  }
